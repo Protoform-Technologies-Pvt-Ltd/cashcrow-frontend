ef27237d7e62208a06721e687586d3bdf31aae3d
Ashique Muhammed <114593982+ashiq-mtn@users.noreply.github.com>
2025-07-03 12:52:53 +0530

    Update firebaseConfig.ts

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -1,17 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdLxz2UEW7tmnDZpUfAbhERJGvv-Aj7h4",
-  authDomain: "cashcrow-v1.firebaseapp.com",
-  projectId: "cashcrow-v1",
-  storageBucket: "cashcrow-v1.firebasestorage.app",
-  messagingSenderId: "333211426416",
-  appId: "1:333211426416:web:8e0ff9b72ffbcdb01bdeb5",
-  measurementId: "G-Q1P1GCS0GP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASEAPIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASEAUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASEPROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASESTORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASEMSGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASEAPPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASEMEASUREMENTID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {auth, provider}
+
